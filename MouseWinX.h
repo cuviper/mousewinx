92f298a85f95937f34a5985db42aa334498ae687
Josh Stone <cuviper@gmail.com>
2010-09-12 11:08:48 -0700

    Move resource.h contents to simply MouseWinX.h

--- MouseWinX.h
+++ MouseWinX.h
@@ -1,3 +1,45 @@
-#pragma once
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by MouseWinX.rc
+//
+#define IDI_HOT                         101
+#define IDI_COLD                        102
+#define IDR_NOTIFYICON                  110
+#define IDD_SETTINGS                    137
+#define IDS_HELP_OK                     142
+#define IDS_HELP_CANCEL                 143
+#define IDS_HELP_APPLY                  144
+#define IDS_HELP_USERGROUP              145
+#define IDS_HELP_USERTRACKING           146
+#define IDS_HELP_USERZORDER             147
+#define IDS_HELP_USERDELAY              148
+#define IDS_HELP_SYSGROUP               149
+#define IDS_HELP_SYSTRACKING            150
+#define IDS_HELP_SYSZORDER              151
+#define IDS_HELP_SYSDELAY               152
+#define IDC_SYSGROUP                    1002
+#define IDC_USERTRACKING                1003
+#define IDC_USERGROUP                   1004
+#define IDC_USERZORDER                  1005
+#define IDC_SYSTRACKING                 1006
+#define IDC_SYSZORDER                   1007
+#define IDC_USERDELAY                   1008
+#define IDC_USERDELAYLABEL              1009
+#define IDC_SYSDELAY                    1010
+#define IDAPPLY                         1012
+#define ID_POPUP_ACTIVATE               32771
+#define ID_POPUP_AUTORAISE              32772
+#define ID_POPUP_SETTINGS               32773
+#define ID_POPUP_EXIT                   32774
 
-#include "resource.h"
+// Next default values for new objects
+//
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        144
+#define _APS_NEXT_COMMAND_VALUE         32775
+#define _APS_NEXT_CONTROL_VALUE         1014
+#define _APS_NEXT_SYMED_VALUE           111
+#endif
+#endif
